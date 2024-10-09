a056661e466ae3aad4f7bdefe24485a27fabb3fd
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2024-10-09 19:36:33 +0900

    Update speech_start.bat

--- windows_operation/speech_start.bat
+++ windows_operation/speech_start.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: Speech.exeのパスを指定
+:: 音声認識の実行書類のパスを指定
 set speechExePath=%SystemRoot%\Speech\Common\sapisvr.exe
 
 :: 引数を指定
 set arguments=-SpeechUX
 
-:: Speech.exeを起動
+:: 音声認識を起動
 start %speechExePath% %arguments%
