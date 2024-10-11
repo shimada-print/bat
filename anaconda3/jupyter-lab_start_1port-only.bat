1c4cda96d9c6ad7059fbf6e3067083d3859de507
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2024-10-11 13:14:07 +0900

    Update jupyter-lab_start_1port-only.bat

--- anaconda3/jupyter-lab_start_1port-only.bat
+++ anaconda3/jupyter-lab_start_1port-only.bat
@@ -1,7 +1,11 @@
+:: Anaconda3のJupyterLabをクリックで起動できるスクリプト
+
+:: WindowsのAnaconda3ではWindowsの改行コード以外では起動できない場合があります。
+:: 本スクリプトをテキストエディターなどで変更してください。
+
+:: BAT宣言
 @echo off
 setlocal
-
-:: Anaconda3のJupyterLabをクリックで起動する
 
 :: JupyterLabのパスを環境変数から取得 (適宜変更)
 :: 環境変数の設定は変数「anaconda3_scripts」変数値「C:\Users\あなたのユーザー名\anaconda3\Scripts」など。
